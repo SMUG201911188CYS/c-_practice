cd5d368945987b208fa7d614a737984e9e545e99
ghs00456@gmail.com <53203389+SMUG201911188CYS@users.noreply.github.com>
2022-08-12 22:29:58 +0900

    테스트

---
+++ test.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b;
+	int result = 0;
+
+	std::cout << "숫자 두개 입력 : ";
+
+	std::cin >> a >> b;
+
+
+	for (; a <= b; a++) {
+		result += a;
+	}
+	
+	std::cout << result;
+
+}
