5e8cca986bab4141bd4f092ac59b7f58a3703b42
SMUG201911188CYS <53203389+SMUG201911188CYS@users.noreply.github.com>
2022-09-01 10:39:04 +0900

    0901

---
+++ 9-4.cpp
@@ -0,0 +1,17 @@
+﻿#include "sales.h"
+#include <iostream>
+
+int main()
+{
+	SALES::Sales n1;
+	SALES::Sales n2;
+
+	double ar[3]{ 1000, 2000.4, 4000.3 };
+
+	SALES::setSales(n1);
+	SALES::setSales(n2, ar, 3);
+
+	SALES::showSales(n1);
+	SALES::showSales(n2);
+
+}
